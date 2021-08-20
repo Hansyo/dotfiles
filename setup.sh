fb357d4888e46ef3fd4672181a374200ffc45246
Hansyo <hansyo.souti.1999@gmail.com>
2021-08-20 16:30:09 +0900

    [Change] setup.sh modified
    管理しやすいように書き換えた

--- setup.sh
+++ setup.sh
@@ -2,39 +2,60 @@
 
 # スクリプトのあるディレクトリを参照する
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+# DOTFILESをここに設定する
+export DOTFILES=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+
+# リンクから除外すべきものはきちんと指定する
+IGNORES=".git|.vim/"
 
 # すべてのディレクトリについて、適切な場所にシンボリックリンクを貼る
-for directory in `\find . -maxdepth 1 -type d`; do
-	# ".",".git"以外のフォルダを見る
-	if ! ( test ${directory} = "." || test ${directory:2} = ".git" ); then
-		# ".config"のみ、再帰っぽく、もう一回おんなじことをやる
-		if test ${directory:2} = ".config"; then
-			cd .config
-			for conf_d in `\find . -maxdepth 1 -type d`; do
-				if ! test ${conf_d} = ".";then
-					if [[ ! -h ${HOME}/.config/${conf_d:2} ]];then
-						echo "linking ${SCRIPT_DIR}/${directory:2}/${conf_d:2} -> ${HOME}/${directory:2}/${conf_d:2}"
-						ln -sd ${SCRIPT_DIR}/${directory:2}/${conf_d:2} ${HOME}/.config/${conf_d:2}
-					fi
-				fi
-			done
-			cd ..
-		else
-			if [[ ! -h ${HOME}/${directory:2} ]];then
-				echo "linking ${SCRIPT_DIR}/${directory:2} -> ${HOME}/${directory:2}"
-				ln -sd ${SCRIPT_DIR}/${directory:2} ${HOME}/${directory:2}
-			fi
-		fi
+DIRS=`find ${SCRIPT_DIR} -mindepth 1 -type d -printf '%P\n'|grep -vE "${IGNORES}"`
+for dir in ${(f)DIRS};do
+	if [[ -e ${HOME}/${dir} ]];then
+		echo "Skip(Exist) ${dir}"
+	else
+		# ディレクトリが存在しない場合のみ、リンク
+		echo "linking ${SCRIPT_DIR}/${dir} -> ${HOME}/${dir}"
+		echo "ln -sd ${SCRIPT_DIR}/${dir} ${HOME}/${dir}"
 	fi
 done
 
-# 更に、個別に必要となりうるものは、全部手動でリンクする
-ln -sf ${SCRIPT_DIR}/.zshrc.d/.zshrc ${HOME}/.zshrc
 
-for f in `\find . -maxdepth 1 -type f`;do
-  str=`echo $f | cut -c 3- `;
-  if ( test `echo $str | cut -c 1` = "." && test $str != ".gitmodules" && test $str != ".gitignore" ); then
-    ln -sf ${SCRIPT_DIR}/$str ${HOME}/$str
-    echo "linking ${SCRIPT_DIR}/$str -> ${HOME}/$str"
-  fi
+# dotfiles直下のファイルをリンク
+IGNORE_FILES=".git"
+FILES=`find ${SCRIPT_DIR} -maxdepth 1 -type f -name '.*' -printf '%P\n'|grep -vE "${IGNORE_FILES}"`
+for f in ${(f)FILES};do
+	if [[ -e ${HOME}/${f} ]];then
+		echo "Skip(Exist) ${f}"
+	else
+		echo "linking ${SCRIPT_DIR}/${f} -> ${HOME}/${f}"
+		ln -sf ${SCRIPT_DIR}/${f} ${HOME}/${f}
+	fi
+done
+
+# 個別ファイルのリンクとコピー
+LINKS=`tail -n -1 ${SCRIPT_DIR}/link.list`
+for f in ${(f)LINKS};do
+	ARY=(`echo ${f} | tr -s ':' ' '`)
+	SOURCE=$(eval echo ${ARY[1]})
+	DEST=$(eval echo ${ARY[2]})
+	if [[ -e ${DEST} ]];then
+		echo "Skip(Exist) ${DEST}"
+	else
+		echo "linking ${SOURCE} -> ${DEST}"
+		ln -sf ${SOURCE} ${DEST}
+	fi
+done
+
+COPYS=`tail -n -1 ${SCRIPT_DIR}/copy.list`
+for f in ${(f)COPYS};do
+	ARY=(`echo ${f} | tr -s ':' ' '`)
+	SOURCE=$(eval echo ${ARY[1]})
+	DEST=$(eval echo ${ARY[2]})
+	if [[ -e ${DEST} ]];then
+		echo "Skip(Exist) ${DEST}"
+	else
+		echo "copying ${SOURCE} -> ${DEST}"
+		cp ${SOURCE} ${DEST}
+	fi
 done
