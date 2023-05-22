1e364bdb2fdf3e3e70cb3eaa7f24d35e785e68b7
Hansyo <hansyo.souti.1999@gmail.com>
2023-05-22 16:24:16 +0900

    update: nvim colorscheme settings

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -1,26 +1,47 @@
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
-local transparentBgGrp = augroup('TransparentBG', {clear = true})
-autocmd('Colorscheme', { pattern = 'nightfly', group = transparentBgGrp, command = 'highlight Normal ctermbg=NONE guibg=NONE'})
-autocmd('Colorscheme', { pattern = 'nightfly', group = transparentBgGrp, command = 'highlight NonText ctermbg=NONE guibg=NONE'})
-autocmd('Colorscheme', { pattern = 'nightfly', group = transparentBgGrp, command = 'highlight LineNr ctermbg=NONE guibg=NONE'})
-autocmd('Colorscheme', { pattern = 'nightfly', group = transparentBgGrp, command = 'highlight CursorLineNr ctermbg=NONE guibg=NONE'})
-autocmd('Colorscheme', { pattern = 'nightfly', group = transparentBgGrp, command = 'highlight Folded ctermbg=NONE guibg=NONE'})
-autocmd('Colorscheme', { pattern = 'nightfly', group = transparentBgGrp, command = 'highlight EndOfBuffer ctermbg=NONE guibg=NONE'})
-autocmd('Colorscheme', { pattern = 'nightfly', group = transparentBgGrp, command = 'highlight VertSplit ctermbg=NONE guibg=NONE'})
+local transparentBgGrp = augroup("TransparentBG", { clear = true })
+local transparentCommand = function(au_cmd_name)
+	return {
+		pattern = "nightfly",
+		group = transparentBgGrp,
+		command = string.format("highlight %s ctermbg=NONE guibg=NONE", au_cmd_name),
+	}
+end
+autocmd("Colorscheme", transparentCommand("Normal"))
+autocmd("Colorscheme", transparentCommand("NonText"))
+autocmd("Colorscheme", transparentCommand("LineNr"))
+autocmd("Colorscheme", transparentCommand("CursorLineNr"))
+autocmd("Colorscheme", transparentCommand("Folded"))
+autocmd("Colorscheme", transparentCommand("EndOfBuffer"))
+autocmd("Colorscheme", transparentCommand("VertSplit"))
+autocmd("Colorscheme", transparentCommand("WinBar"))
+autocmd("Colorscheme", transparentCommand("WinBarNC"))
 
-local highlightLineGrp = augroup('HighlightLine', {clear = true})
-autocmd('Colorscheme', { pattern = 'nightfly', group = highlightLineGrp, command = 'highlight LineNr ctermfg=3 guifg=#909b1b'})
-autocmd('Colorscheme', { pattern = 'nightfly', group = highlightLineGrp, command = 'highlight CursorLineNr ctermfg=Yellow guifg=#f8fa1c'})
+local highlightLineGrp = augroup("HighlightLine", { clear = true })
+autocmd("Colorscheme", {
+	pattern = "nightfly",
+	group = highlightLineGrp,
+	command = "highlight LineNr ctermfg=3 guifg=#909b1b",
+})
+autocmd("Colorscheme", {
+	pattern = "nightfly",
+	group = highlightLineGrp,
+	command = "highlight CursorLineNr ctermfg=Yellow guifg=#f8fa1c",
+})
 
-local highlightLspGrp = augroup('HighlightLsp', {clear = true})
-autocmd('Colorscheme', { pattern = 'nightfly', group = highlightLspGrp, command = 'highlight lspReference ctermfg=white guifg=#FFFFFF ctermbg=6 guibg=#008080'})
+local highlightLspGrp = augroup("HighlightLsp", { clear = true })
+autocmd("Colorscheme", {
+	pattern = "nightfly",
+	group = highlightLspGrp,
+	command = "highlight lspReference ctermfg=white guifg=#FFFFFF ctermbg=6 guibg=#008080",
+})
 
-vim.api.nvim_set_option('termguicolors', true)
+vim.api.nvim_set_option("termguicolors", true)
 vim.g.nightflyCursorColor = true
 vim.g.nightflyUnderlineMatchParen = true
 vim.g.nightflyTransparent = true
 vim.g.nightflyUndercurls = true
 
-vim.cmd [[ colorscheme nightfly ]]
+vim.cmd([[ colorscheme nightfly ]])
