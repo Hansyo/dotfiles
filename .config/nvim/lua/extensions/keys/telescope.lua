4526db5115a77536132b2067963ad5278e829804
Hansyo <hansyo.souti.1999@gmail.com>
2023-05-25 22:43:13 +0900

    fix: neovim Telescope key shortcut

--- .config/nvim/lua/extensions/keys/telescope.lua
+++ .config/nvim/lua/extensions/keys/telescope.lua
@@ -1,10 +1,14 @@
-return {
-	{ "<leader>f",                "<Plug>(TelescopeLeader)" },
-	{ "<Plug>(TelescopeLeader)j", "Telescope current_buffer_fuzzy_find", },
-	{ "<Plug>(TelescopeLeader)f", "Telescope find_files", },
-	{ "<Plug>(TelescopeLeader)F", "Telescope find_files hidden=true", },
-	{ "<Plug>(TelescopeLeader)g", "Telescope live_grep", }, -- This will need ripgrep
-	{ "<Plug>(TelescopeLeader)b", "Telescope buffers", },
-	{ "<Plug>(TelescopeLeader)h", "Telescope help_tags", },
+local TeleLeader = "<Plug>(TeleLeader)"
+local wrap = function(cmd)
+	return "<Cmd>Telescope " .. cmd .. "<CR>"
+end
 
+return {
+	{ "<Leader>f",       TeleLeader },
+	{ TeleLeader .. "j", wrap("current_buffer_fuzzy_find"), },
+	{ TeleLeader .. "f", wrap("find_files"), },
+	{ TeleLeader .. "F", wrap("find_files hidden=true"), },
+	{ TeleLeader .. "g", wrap("live_grep"), }, -- This will need ripgrep
+	{ TeleLeader .. "b", wrap("buffers"), },
+	{ TeleLeader .. "h", wrap("help_tags"), },
 }
