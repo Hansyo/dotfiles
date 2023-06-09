dec5a4bc321795eeeb66099bef6cbe3aa8958aab
Hansyo <hansyo.souti.1999@gmail.com>
2023-06-09 10:51:35 +0900

    update: nvim noice settings

--- .config/nvim/lua/extensions/config/noice.lua
+++ .config/nvim/lua/extensions/config/noice.lua
@@ -1,9 +1,9 @@
 return function()
 	require("noice").setup({
 		cmdline = {
-			enabled = true,      -- enables the Noice cmdline UI
+			enabled = true, -- enables the Noice cmdline UI
 			view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
-			opts = {             -- global options for the cmdline. See section on views
+			opts = {       -- global options for the cmdline. See section on views
 				position = "50%",
 			},
 			---@diagnostic disable-next-line: undefined-doc-name
@@ -15,13 +15,24 @@ return function()
 				-- icon_hl_group: optional hl_group for the icon
 				-- title: set to anything or empty string to hide
 				cmdline = { pattern = "^:", icon = "", lang = "vim" },
-				search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
-				search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+				search_down = {
+					kind = "search",
+					pattern = "^/",
+					icon = " ",
+					lang = "regex",
+					opts = { position = "100%" },
+				},
+				search_up = {
+					kind = "search",
+					pattern = "^%?",
+					icon = " ",
+					lang = "regex",
+					opts = { position = "100%" },
+				},
 				filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
 				lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
-				help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+				help = { pattern = "^:%s*he?l?p?%s+" },
 				input = {}, -- Used by input()
-				-- lua = false, -- to disable a format, set to `false`
 			},
 		},
 		lsp = {
@@ -32,11 +43,11 @@ return function()
 			},
 		},
 		presets = {
-			bottom_search = true,      -- use a classic bottom cmdline for search
-			command_palette = true,    -- position the cmdline and popupmenu together
+			bottom_search = true, -- use a classic bottom cmdline for search
+			command_palette = true, -- position the cmdline and popupmenu together
 			long_message_to_split = true, -- long messages will be sent to a split
-			inc_rename = false,        -- enables an input dialog for inc-rename.nvim
-			lsp_doc_border = false,    -- add a border to hover docs and signature help
+			inc_rename = false,  -- enables an input dialog for inc-rename.nvim
+			lsp_doc_border = false, -- add a border to hover docs and signature help
 		},
 	})
 end
