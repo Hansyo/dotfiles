3b594cf223a0335ee1934852cb8e3cf725d39603
Hansyo <hansyo.souti.1999@gmail.com>
2023-06-21 13:02:04 +0900

    update: zsh options

---
+++ .config/zsh/alias.zsh
@@ -0,0 +1,12 @@
+_echo '  start alias'
+# alias関連
+#alias touchpad-disable='xinput disable 12'
+#alias touchpad-enabe='xinput enable 12'
+#alias touchpad-on-off='touchpad-on-off.sh'
+alias reload='exec zsh'
+alias more='\less -R'
+alias rm='\rm -i'
+alias cp='\cp -i'
+alias ls='\ls --color=always'
+alias up='cd ..'
+
--- .config/zsh/options.zsh
+++ .config/zsh/options.zsh
@@ -1,27 +1,15 @@
 _echo '  start set PATH'
-## export(PATH)関連
+## export関連
 export PYTHONPATH=/usr/local/lib/python3.6/dist-packages:$PYTHONPATH
-export XDG_CONFIG_HOME=~/.config
 export PREFIX="$HOME/opt/cross"
 export TARGET=i686-elf
 export PGDATA=/usr/local/var/postgres
 export LESS='-R' #lessコマンドを常時カラー表示できるようにする
 export LANGUAGE=ja
-export DOTFILES=${HOME}/dotfiles # dotfilesを設定しておく
+export EDITOR='nvim'
+export VISUAL='nvim'
 
-_echo '  start alias'
-# alias関連
-#alias touchpad-disable='xinput disable 12'
-#alias touchpad-enabe='xinput enable 12'
-#alias touchpad-on-off='touchpad-on-off.sh'
-alias reload='exec zsh'
-alias more='\less -R'
-alias rm='\rm -i'
-alias cp='\cp -i'
-alias ls='\ls --color=always'
-
-_echo '  start compinit'
-## 関連して、間違ったら修正を表示
+## 間違ったら修正を提案
 setopt correct
 
 # ヒストリの設定
@@ -34,7 +22,6 @@ HISTFILE=$ZDOTDIR/.zsh_history
 ### 個数
 HISTSIZE=1000000
 SAVEHIST=1000000
-
 
 ### 重複したら古いコマンドを消す
 #### どちらかを選択すれば良い
@@ -50,7 +37,6 @@ setopt hist_no_store
 ### 余分な空白は詰める
 setopt hist_reduce_blanks
 
-_echo '  start pushd'
 # cdしたら自動でpushdする
 setopt auto_pushd
 
@@ -58,12 +44,4 @@ _echo '  start bindkey'
 # bindey関連
 ## emacs風のキーバインド
 bindkey -e
-
-## その他のキーバインド
-
-_echo '  start user_Made'
-
-# exportのもろもろ
-export EDITOR='nvim'
-export VISUAL='nvim'
 
