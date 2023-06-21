d5a6802114218d709ddaa73d2cd4a52f0e0ab807
Hansyo <hansyo.souti.1999@gmail.com>
2023-06-21 13:24:13 +0900

    fix: zsh Darwin's coreutil path

--- .config/zsh/os/Darwin.zsh
+++ .config/zsh/os/Darwin.zsh
@@ -3,11 +3,11 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 export LSCOLORS_ARRAY=(gx fx Cx dx Bx eg ed ab ag ac ad)
 export LSCOLORS=${(j::)LSCOLORS_ARRAY}
-zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 
-if [ -d /usr/local/opt/coreutils/libexec/gnubin ]; then
-  export PATH=/usr/local/opt/coreutils/libexec/gnubin:$PATH
-  export MANPATH=/usr/local/opt/coreutils/libexec/gnuman:$MANPATH
+COREUTILS_LIBEXEC_PATH="/opt/homebrew/opt/coreutils/libexec"
+if [ -d $COREUTILS_LIBEXEC_PATH ]; then
+  export PATH=$COREUTILS_LIBEXEC_PATH/gnubin:$PATH
+  export MANPATH=$COREUTILS_LIBEXEC_PATH/gnuman:$MANPATH
 fi
 
 PIPR_PATH="~/.local/share/pipr/pipr_macos.zsh"
