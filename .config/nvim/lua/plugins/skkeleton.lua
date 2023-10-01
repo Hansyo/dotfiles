7cddbe013b62c18e6a46f379377a0a53de8c8d2d
Hansyo <hansyo.souti.1999@gmail.com>
2023-10-01 21:56:49 +0900

    update: skkeleton key table

--- .config/nvim/lua/plugins/skkeleton.lua
+++ .config/nvim/lua/plugins/skkeleton.lua
@@ -7,15 +7,31 @@ return {
 		},
 		keys = {
 			{ "<C-j>", "<Plug>(skkeleton-enable)", mode = { "c", "i" } },
+			{ "kk", "<Plug>(skkeleton-enable)", mode = { "c", "i" } },
 			{ "<C-k>", "<Plug>(skkeleton-disable)", mode = { "c", "i" } },
 			{ "<C-f>", "<Plug>(skkeleton-toggle)", mode = { "c", "i" } },
 		},
 
 		config = function()
 			vim.fn["skkeleton#config"]({
-			globalJisyo = "~/.skk/SKK-JISYO.L",
+				globalJisyo = "~/.skk/SKK-JISYO.L",
 				userJisyo = "~/.skk/USER-JISYO",
+				useSkkServer = true,
 				eggLikeNewline = true, -- 変換モードで改行したときに、確定だけ行う(改行しない)
+			})
+			vim.fn["skkeleton#register_kanatable"]("rom", {
+				jj = "escape",
+				ll = "disable",
+				la = { "ぁ" },
+				li = { "ぃ" },
+				lu = { "ぅ" },
+				le = { "ぇ" },
+				lo = { "ぉ" },
+				lya = { "ゃ" },
+				lyu = { "ゅ" },
+				lyo = { "ょ" },
+				ltu = { "っ" },
+				ltsu = { "っ" },
 			})
 		end,
 	},
