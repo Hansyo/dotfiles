4924ab11cd4c03fbe93ede03b04347517aace1f1
Hansyo <hansyo.souti.1999@gmail.com>
2024-03-21 17:31:34 +0900

    add: raycast settings

---
+++ .config/raycast/scripts/alacritty-new-window.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Create new Alacritty window
+# @raycast.packageName Alacritty
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon /Applications/Alacritty.app/Contents/Resources/alacritty.icns
+
+# Documentation:
+# @raycast.author Hansyo
+# @raycast.authorURL https://github.com/Hansyo
+
+alacritty msg create-window
---
+++ .config/raycast/scripts/open-wezterm-new-window.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open WezTerm new window
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon /Applications/WezTerm.app/Contents/Resources/terminal.icns
+
+# Documentation:
+# @raycast.author Hansyo
+# @raycast.authorURL https://github.com/Hansyo
+
+wezterm start
